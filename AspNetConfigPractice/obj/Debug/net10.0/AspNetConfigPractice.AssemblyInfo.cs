b685c4fc975f231a320377c7667a444b750f30ab
Black287234 <arkasha.khromov@vk.com>
2026-09-22 06:26:23 +0000

    chore: настроен автозапуск на api/students и обновлен http файл

--- AspNetConfigPractice/obj/Debug/net10.0/AspNetConfigPractice.AssemblyInfo.cs
+++ AspNetConfigPractice/obj/Debug/net10.0/AspNetConfigPractice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AspNetConfigPractice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f27024a16a6ca2b6d745dbd1e315fb4a0ae8eb4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a073190b154e26d9f83339b1de16ee48fb7d44c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("AspNetConfigPractice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AspNetConfigPractice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
